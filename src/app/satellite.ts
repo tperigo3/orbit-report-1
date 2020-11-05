9e4dddb7b13c1589fe1a374c07629c83fe7a90a8
Garner Perigo <tperigo3@gmail.com>
2020-11-04 20:35:36 -0600

    finished section 2

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,13 +4,12 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+    
     constructor (name:string, type:string, launchDate:string, orbitType: string, operational: boolean){
-        this.sourceList = [
-            new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-            new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-            new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-            new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-            new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-        ];
+      this.name = name;
+      this.orbitType = orbitType;
+      this.type = type;
+      this.operational = operational;
+      this.launchDate = launchDate;
     }    
 }
